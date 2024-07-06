3575486b5d87a740728f9ae9c0014ea43d5dec50
alan caceres medrano <202009972@est.umss.edu>
2024-07-05 20:42:44 -1100

    todo terminado

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const vocales = ['a','e','i','o','u'];
 const valores = ["ai","enter","imes","ober","ufat"];
+let contador=0;
 
 function desencriptar(){
     let texto = document.getElementById("texto_encriptar").value;
@@ -20,6 +21,37 @@ function desencriptar(){
     }
     limpiar();
     document.getElementById("texto_encriptado").value = desencriptado;
+
+}
+function tieneMayusculas(texto){
+    let letra="";
+    let res=false;
+      for(let i=0;i<texto.length;i++){
+         letra=texto[i];
+         if(letra === letra.toUpperCase()){
+            res = true;
+            break;
+         }
+      }
+      console.log(res);
+      return res;
+}
+function tieneAcentos(texto){
+    let regex = /[áéíóúÁÉÍÓÚ]/;
+    let letra="";
+    let res=false;
+      for(let i=0;i<texto.length;i++){
+         letra=texto[i];
+         if(regex.test(letra)){
+            res=true;
+         }
+      }
+      console.log(res);
+    return res;
+}
+function mostrarMensajeError(){
+    let elemento = document.getElementById("id_error");
+    elemento.innerHTML = ` Solo letras minusculas y sin acentos`;
 }
 
 function encriptar(){
@@ -27,18 +59,36 @@ function encriptar(){
     let letra="";
     let encriptado="";
     let indice;
-    for(let i=0;i<texto.length;i++){
-        letra = texto[i];
-        if(existeEnVocales(letra)){
-            indice = buscarIndice(letra);
-            encriptado += valores[indice];
-        }else{
-            encriptado += letra;
+    if(!tieneMayusculas(texto) && !tieneAcentos(texto)){
+        for(let i=0;i<texto.length;i++){
+            letra = texto[i];
+            if(existeEnVocales(letra)){
+                indice = buscarIndice(letra);
+                encriptado += valores[indice];
+            }else{
+                encriptado += letra;
+            }
         }
+        if(contador==0){
+            agregarElementos();
+            contador++;
+        }
+        document.getElementById("id_error").innerHTML='';
+        document.getElementById('boton_desencriptar').removeAttribute('disabled');
+        limpiar();
+        document.getElementById("texto_encriptado").value = encriptado;
+    }else{
+        mostrarMensajeError();
     }
-    limpiar();
-    document.getElementById("texto_encriptado").value = encriptado;
 }
+function agregarElementos(){
+    document.getElementById("mensaj_inicial").remove();
+    document.getElementById("contenedor2").innerHTML += `
+        <textarea class="texto_area2"  id="texto_encriptado"></textarea>
+        <button class="boton_terciario" onclick="copiarTexto();">copiar</button>
+    `;
+}
+
 function existeEnVocales(letra){
     let res=false;
     for(let i=0;i<vocales.length;i++){
